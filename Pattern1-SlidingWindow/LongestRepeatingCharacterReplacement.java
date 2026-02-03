9bbf48213a86d5f76ad6beaec876c0a0e40eda0d
Aryan Srivastava <srivastavaaryan60@gmail.com>
2026-02-03 16:08:01 +0530

    Longest Repeating Character Replacement

---
+++ Pattern1-SlidingWindow/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,21 @@
+//Link : https://leetcode.com/problems/longest-repeating-character-replacement/
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int freq[] = new int[26];
+        int left =0;
+        int maxCount =0,maxLen=0;
+        for(int right =0;right<s.length();right++){
+            char ch = s.charAt(right);
+            freq[ch-'A']++;
+            maxCount=Math.max(maxCount,freq[ch-'A']);
+
+            while((right-left+1)-maxCount>k){
+                freq[s.charAt(left)-'A']--;
+                left++;
+            }
+            maxLen = Math.max(maxLen,right-left+1);
+        }
+        return maxLen;
+    }
+}
