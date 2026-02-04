0ac23742aa9b921c7b2c7b777b55cce3bb190538
Aryan Srivastava <srivastavaaryan60@gmail.com>
2026-02-04 13:23:53 +0530

    Permutation in String

---
+++ Pattern1-SlidingWindow/PermutationinString.java
@@ -0,0 +1,32 @@
+//Link : https://leetcode.com/problems/permutation-in-string/
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()) return false;
+        int freq1[] = new int[26];
+        int freq2[] = new int[26];
+
+        for(char ch : s1.toCharArray()){
+            freq1[ch-'a']++;
+        }
+        int left =0;
+
+        for(int right=0;right<s2.length();right++){
+            freq2[s2.charAt(right)-'a']++;
+            if(right-left+1>s1.length()){
+                freq2[s2.charAt(left)-'a']--;
+                left++;
+            }
+
+            if(right-left+1==s1.length()){
+                if(matches(freq1,freq2)) return true;
+            }
+        }
+        return false;
+    }
+    private boolean matches(int[]a,int[]b){
+        for(int i=0;i<26;i++){
+            if(a[i]!=b[i])return false;
+        }
+        return true;
+    }
+}
