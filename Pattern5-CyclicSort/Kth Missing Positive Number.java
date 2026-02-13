f25a6ddb897ec7819ca989e414210c028578c00f
Aryan Srivastava <srivastavaaryan60@gmail.com>
2026-02-13 23:11:58 +0530

    Kth Missing Positive Number

---
+++ Pattern5-CyclicSort/Kth Missing Positive Number.java
@@ -0,0 +1,18 @@
+//Link: https://leetcode.com/problems/kth-missing-positive-number/description/
+
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int curr =1;
+        int i=0;
+        while(k>0){
+            if(i<arr.length && arr[i]==curr){
+                i++;
+            }else{
+                k--;
+                if(k==0) return curr;
+            }
+            curr++;
+        }
+        return -1;
+    }
+}
