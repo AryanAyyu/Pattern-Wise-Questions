5ce28382d986063cc89af5487a0d952944b94cf0
Aryan Srivastava <srivastavaaryan60@gmail.com>
2026-02-14 17:44:41 +0530

    Find Bottom Left Tree Value

---
+++ Pattern7-TreeBreadthFirstSearch/Find Bottom Left Tree Value.java
@@ -0,0 +1,38 @@
+//Link: https://leetcode.com/problems/find-bottom-left-tree-value/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        int val = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode node = queue.poll();
+                if(i==0) val = node.val;
+                if(node.left!=null){
+                    queue.offer(node.left);
+                }
+                if(node.right!=null){
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return val;
+    }
+}
